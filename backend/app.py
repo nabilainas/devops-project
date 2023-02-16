e37ff40c4889abcd42341a7393ffbd400058156a
nabilainas <ainaspro@gmail.com>
2023-02-16 23:20:31 +0100

    Finishing unit test, cleaning code

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask,request, jsonify
+from flask import Flask,request, jsonify, make_response
 from flask_cors import CORS
 from pymongo import MongoClient
+
 
 app = Flask(__name__)
 CORS(app)
@@ -10,25 +11,26 @@ collection = dbname['employees']
 
 @app.route("/api/v1/employees", methods=['GET'])
 def get_employees():
-    return jsonify([i for i in collection.find({}, {"_id":0})]), 200
+    return jsonify([i for i in collection.find({}, {"_id": 0 })]), 200
 
 @app.route("/api/v1/employees", methods=['GET','POST'])
 def add_employee():
     data = request.get_json()
-    data["id"] = len([i for i in collection.find({}, {"_id":0})]) + 1
-    collection.insert_one(data)
-    return jsonify(data), 200
+    data["id"] = len([i for i in collection.find({}, {"_id": 0 })]) + 1
+    collection.insert_one(data).inserted_id
+    del data["_id"]
+    return jsonify(data), 201
 
 @app.route("/api/v1/employees/<int:id>", methods=['GET'])
 def get_employee(id):
-    return [element for element in [i for i in collection.find({}, {"_id":0})] if element["id"] == id][0]
+    return [element for element in [i for i in collection.find({}, {"_id":0})] if element["id"] == id][0], 200
 
 @app.route("/api/v1/employees/<int:id>", methods=['GET','PUT'])
 def update_employee(id):
     result = collection.update_one({"id": int(id)}, {"$set": request.get_json()})
-    return jsonify({"result": "Employee updated"}) if result.modified_count == 1 else jsonify({"error": "Employee not found"}) 
+    return make_response(jsonify({'result': 'Employee updated'}), 204) if result.modified_count == 1 else make_response(jsonify({'error': 'Employee not found'}), 400)
 
 @app.route("/api/v1/employees/<int:id>", methods=['GET','DELETE'])
 def delete_employee(id):
     result = collection.delete_one({'id': int(id)})
-    return jsonify({'result': 'Employee deleted'}) if result.deleted_count == 1 else jsonify({'error': 'Employee not found'})
+    return make_response(jsonify({'result': 'Employee deleted'}), 204) if result.deleted_count == 1 else make_response(jsonify({'error': 'Employee not found'}), 400)
---
+++ backend/test.py
@@ -0,0 +1,51 @@
+import unittest
+import requests
+
+class TestEmployeeAPI(unittest.TestCase):
+
+    def setUp(self):
+        self.base_url = 'http://localhost:8080/api/v1/employees'
+        self.headers = {'Content-Type': 'application/json'}
+        self.employee = {
+            "firstName": "nabil",
+            "lastName": "ainas",
+            "emailId": "nabil@ainas",
+        }
+        res = requests.get(self.base_url)
+        self.id_test = len(res.json())
+        print(f'first  : {self.id_test}')
+
+    def test_1_get_employees(self):
+        response = requests.get(self.base_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_2_get_employee_by_id(self):
+        response = requests.get(f'{self.base_url}/{self.id_test}')
+        print(f"get : {response.json()}")
+        print(f'get id  : {self.id_test}')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_1_post_employee(self):
+        response = requests.post(self.base_url, headers=self.headers, json=self.employee)
+        print(f"post : {response.json()}")
+        self.assertEqual(response.status_code, 201)
+
+    def test_4_put_employee_by_id(self):
+        updated_employee = {
+            "firstName": "nabileboss",
+            "lastName": "ainas",
+            "emailId": "leboss@ainas",
+        }
+        response = requests.put(f'{self.base_url}/{self.id_test}', json=updated_employee)
+        print(f"put : {response.text}")
+        print(f'put id  : {self.id_test}')
+        self.assertEqual(response.status_code, 204)
+    
+    def test_5_delete_employee_by_id(self):
+        response = requests.delete(f'{self.base_url}/{self.id_test}')
+        print(f' deleted ')
+        print(f'del id  : {self.id_test}')
+        self.assertEqual(response.status_code, 204)
+
+if __name__ == '__main__':
+    unittest.main()
